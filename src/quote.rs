65d349f9fbb9e39da80ded8467e8dfc893bab33f
soqb <cb.setho@gmail.com>
2025-12-19 17:26:08 +0000

    feat(quote): context-aware macro revision 1 (i think it works yippee)

--- src/quote.rs
+++ src/quote.rs
@@ -166,29 +166,100 @@ macro_rules! ඞ_macro_extend_quote_size_reservation {
 
 #[doc(hidden)]
 #[macro_export]
-macro_rules! ඞ_macro_extend_quote_impl {
-    ($buf:ident $s:lifetime) => {};
-    ($buf:ident $s:lifetime @@ $($r:tt)*) => {
-        m::push_punct($buf, &['@']);
-        $crate::ඞ_macro_extend_quote_impl! { $buf $s $($r)* }
+macro_rules! ඞ_macro_quote_window6 {
+    ($buf:ident $s:lifetime {@} {@} {@} $_3:tt $_4:tt $_5:tt) => {
+        core::compile_error!("the syntax `@@@` is not supported by `vermouth::quote`.");
     };
-    ($buf:ident $s:lifetime @ $name:ident $($r:tt)*) => {
-        if let TtResult::Err(e) = $name.try_extend_tokens($buf) {
+    ($buf:ident $s:lifetime {@} {@} $a:tt $b:tt $c:tt $d:tt) => {
+        $crate::ඞ_macro_quote_tt_impl! { $buf $s {@} };
+        $crate::ඞ_macro_quote_window6! { $buf $s _ _ _ _ _ $a }
+        $crate::ඞ_macro_quote_window6! { $buf $s _ _ _ _ $a $b }
+        $crate::ඞ_macro_quote_window6! { $buf $s _ _ _ $a $b $c }
+        $crate::ඞ_macro_quote_window6! { $buf $s _ _ $a $b $c $d }
+    };
+    ($buf:ident $s:lifetime $_0:tt {@} {$n:ident} {($($t:tt)*)} {*} $a:tt) => {
+        core::compile_error!("i'm working on it trust bro");
+        $crate::ඞ_macro_quote_window6! { $buf $s _ _ _ _ _ $a }
+    };
+    ($buf:ident $s:lifetime $_0:tt {@} {$n:ident} {($($t:tt)*)} {$sep:tt} {*}) => {
+        core::compile_error!("i'm working on it trust bro");
+    };
+    ($buf:ident $s:lifetime $_0:tt {@} {$n:ident} $a:tt $b:tt $c:tt) => {
+        if let TtResult::Err(e) = $n.try_extend_tokens($buf) {
             break $s m::err(e);
         }
-        $crate::ඞ_macro_extend_quote_impl! { $buf $s $($r)* }
+        $crate::ඞ_macro_quote_window6! { $buf $s _ _ _ _ _ $a }
+        $crate::ඞ_macro_quote_window6! { $buf $s _ _ _ _ $a $b }
+        $crate::ඞ_macro_quote_window6! { $buf $s _ _ _ $a $b $c }
     };
-    ($buf:ident $s:lifetime @ $extra:tt $($r:tt)*) => {
+    ($buf:ident $s:lifetime $_0:tt {@} {@} $a:tt $b:tt $c:tt) => {};
+    ($buf:ident $s:lifetime $_0:tt {@} $_2:tt $_3:tt $_4:tt $_5:tt) => {
         core::compile_error!(core::concat!(
             "invalid quasi-quoting syntax: `", core::stringify!($extra), "` following `@` is reserved.\n\
             help: use `@@` to quote a single `@` symbol.\n\
             help: see `vermouth::try_quote` for documentation."
         ));
-        $crate::ඞ_macro_extend_quote_impl! { $buf $s $($r)* }
     };
-    ($buf:ident $s:lifetime $o:tt $($r:tt)*) => {
-        $crate::ඞ_macro_quote_tt_impl!($buf $s $o);
-        $crate::ඞ_macro_extend_quote_impl! { $buf $s $($r)* }
+    ($buf:ident $s:lifetime $a:tt $b:tt {@} $_3:tt $_4:tt $_5:tt) => {
+        $crate::ඞ_macro_quote_window6! { $buf $s $a $b _ _ _ _ }
+    };
+    ($buf:ident $s:lifetime $a:tt $b:tt $c:tt {@} $_4:tt $_5:tt) => {
+        $crate::ඞ_macro_quote_window6! { $buf $s $a $b $c _ _ _ }
+    };
+    ($buf:ident $s:lifetime $a:tt $b:tt $c:tt $d:tt {@} $_5:tt) => {
+        $crate::ඞ_macro_quote_window6! { $buf $s $a $b $c $d _ _ }
+    };
+    ($buf:ident $s:lifetime $a:tt $b:tt $c:tt $d:tt $e:tt {@}) => {
+        $crate::ඞ_macro_quote_window6! { $buf $s $a $b $c $d $e _ }
+    };
+    ($buf:ident $s:lifetime $_0:tt $_1:tt $_2:tt $_3:tt $_4:tt $t:tt) => {
+        $crate::ඞ_macro_quote_tt_impl! { $buf $s $t };
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! ඞ_macro_quote_matrix6 {
+    (
+        $buf:ident
+        $s:lifetime
+        { $($a:tt)* }
+        { $($b:tt)* }
+        { $($c:tt)* }
+        { $($d:tt)* }
+        { $($e:tt)* }
+        { $($f:tt)* }
+    ) => {
+        $(
+            $crate::ඞ_macro_quote_window6! { $buf $s $a $b $c $d $e $f }
+        )*
+    };
+}
+#[doc(hidden)]
+#[macro_export]
+macro_rules! ඞ_macro_extend_quote_impl {
+    ($buf:ident $s:lifetime) => {};
+    ($buf:ident $s:lifetime @) => {
+        core::compile_error!("invalid quasi-quoting syntax: `@` cannot trail the input.");
+    };
+    ($buf:ident $s:lifetime $t:tt) => {
+        $crate::ඞ_macro_quote_tt_impl! { $buf $s {$t} };
+    };
+    ($buf:ident $s:lifetime @ $n:ident) => {
+        if let TtResult::Err(e) = $n.try_extend_tokens($buf) {
+            break $s m::err(e);
+        }
+    };
+    ($buf:ident $s:lifetime $($t:tt)*) => {
+        $crate::ඞ_macro_quote_matrix6! {
+            $buf $s
+            { _ _ _ _ _ $({$t})* }
+            { _ _ _ _ $({$t})* _ }
+            { _ _ _ $({$t})* _ _ }
+            { _ _ $({$t})* _ _ _ }
+            { _ $({$t})* _ _ _ _ }
+            { $({$t})* _ _ _ _ _ }
+        }
     };
 }
 
@@ -202,15 +273,13 @@ macro_rules! ඞ_macro_quote_punct_seq {
     };
 }
 
-// FIXME: a dtolnay/quote-style context-aware macro would likely be significantly more performant for long streams.
-//     look into breaking up a stream into consecutive extend_quote calls (or equivalent).
 macro_rules! def_quote_tt {
     (arms = {$($arm:tt)*}, $($p:tt)*) => {
         #[macro_export]
         #[doc(hidden)]
         macro_rules! ඞ_macro_quote_tt_impl_ {
             $(
-                ($buf:ident $s:lifetime $p) => {
+                ($buf:ident $s:lifetime {$p}) => {
                     m::push_punct(
                         $buf,
                         $crate::punct_decompose!(
@@ -239,27 +308,28 @@ macro_rules! def_quote_tt {
 for_all_punct_seqs!(
     def_quote_tt,
     arms = {
-        ($buf:ident $s:lifetime ($($t:tt)*)) => {
+        ($buf:ident $s:lifetime _) => {};
+        ($buf:ident $s:lifetime {($($t:tt)*)}) => {
             $crate::TokenBuf::open_group($buf, proc_macro::Delimiter::Parenthesis);
             $crate::ඞ_macro_extend_quote_impl! { $buf $s $($t)* };
             $crate::TokenBuf::close_group($buf);
         };
-        ($buf:ident $s:lifetime {$($t:tt)*}) => {
+        ($buf:ident $s:lifetime {{$($t:tt)*}}) => {
             $crate::TokenBuf::open_group($buf, proc_macro::Delimiter::Brace);
             $crate::ඞ_macro_extend_quote_impl! { $buf $s $($t)* };
             $crate::TokenBuf::close_group($buf);
         };
-        ($buf:ident $s:lifetime [$($t:tt)*]) => {
+        ($buf:ident $s:lifetime {[$($t:tt)*]}) => {
             $crate::TokenBuf::open_group($buf, proc_macro::Delimiter::Bracket);
             $crate::ඞ_macro_extend_quote_impl! { $buf $s $($t)* };
             $crate::TokenBuf::close_group($buf);
         };
-        ($buf:ident $s:lifetime $id:ident) => {
+        ($buf:ident $s:lifetime {$id:ident}) => {
             if let TtResult::Err(e) = const { m::parse_ident(stringify!($id)) }.try_extend_tokens($buf) {
                 break $s m::err(e);
             }
         };
-        ($buf:ident $s:lifetime $lit:literal) => {
+        ($buf:ident $s:lifetime {$lit:literal}) => {
             if let TtResult::Err(e) = m::Spec::new(&$lit).ඞ_lit_quote::<{ m::parse_lit_regime(core::stringify!($lit)) }>(
                 &$lit,
                 core::stringify!($lit),
